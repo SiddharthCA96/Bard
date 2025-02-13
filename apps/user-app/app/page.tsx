f1571913498dcdb0948414b6393d0c6bdebbfd93
SiddharthCA96 <ss6156852@gmail.com>
2025-02-13 23:53:49 +0530

    sigin linked

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,13 +1,12 @@
 "use client"
 import { signIn, signOut, useSession } from "next-auth/react";
-import { Appbar } from "@repo/ui/appbar";
 import { JSX } from "react";
 import {Navbar} from "@repo/ui/navbar";
 export default function Page(): JSX.Element {
   const session = useSession();
   return (
    <div>
-      <Navbar/>
+      <Navbar onSignin={signIn} onSignout={signOut} user={session.data?.user}/>
    </div>
   );
 }
--- packages/ui/src/Navbar.tsx
+++ packages/ui/src/Navbar.tsx
@@ -1,15 +1,19 @@
 "use client";
 import Link from "next/link";
 import ThemeChanger from "./DarkSwitch";
-import Image from "next/image"
+import Image from "next/image";
+import { Button } from "./button";
+interface NavbarProps {
+  user?: {
+    name?: string | null;
+  };
+  // TODO: can u figure out what the type should be here?
+  onSignin: any;
+  onSignout: any;
+}
 
-export const Navbar = () => {
-  const navigation = [
-    "Features",
-    "Pricing",
-    "Company",
-    "Blog",
-  ];
+export const Navbar = ({ user, onSignin, onSignout }: NavbarProps) => {
+  const navigation = ["Features", "Pricing", "Company", "Blog"];
 
   return (
     <div className="w-full">
@@ -17,7 +21,7 @@ export const Navbar = () => {
         {/* Logo  */}
         <Link href="/">
           <span className="flex items-center space-x-2 text-2xl font-medium text-indigo-500 dark:text-gray-100">
-              {/* <span>
+            {/* <span>
                 <Image
                   src="/assets/letter-b.jpg"
                   width="32"
@@ -32,29 +36,27 @@ export const Navbar = () => {
 
         {/* Login */}
         <div className="gap-3 nav__item mr-2 lg:flex ml-auto lg:ml-0 lg:order-2">
-            <ThemeChanger/>
-            <div className="hidden mr-3 lg:flex nav__item">
-              <Link href="/" className="px-6 py-2 text-white bg-indigo-600 rounded-md md:ml-5">
-                Login
-              </Link>
-            </div>
+          <ThemeChanger />
+          <div className="hidden mr-3 lg:flex nav__item">
+          <Button onClick={user ? onSignout : onSignin}>{user ? "Logout" : "Login"}</Button>
+          </div>
         </div>
-            
-        
+
         {/* menu  */}
         <div className="hidden text-center lg:flex lg:items-center">
           <ul className="items-center justify-end flex-1 pt-6 list-none lg:pt-0 lg:flex">
             {navigation.map((menu, index) => (
               <li className="mr-3 nav__item" key={index}>
-                <Link href="/" className="inline-block px-4 py-2 text-lg font-normal text-gray-800 no-underline rounded-md dark:text-gray-200 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-gray-800">
-                    {menu}
+                <Link
+                  href="/"
+                  className="inline-block px-4 py-2 text-lg font-normal text-gray-800 no-underline rounded-md dark:text-gray-200 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-gray-800">
+                  {menu}
                 </Link>
               </li>
             ))}
           </ul>
         </div>
-
       </nav>
     </div>
   );
-}
+};
